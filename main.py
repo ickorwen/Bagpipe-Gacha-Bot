da0d48505f100e39f5f490b402390e862649e3a0
Ickorwen <snickerweeniespace2006@gmail.com>
2026-04-02 23:08:04 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+
+# Load the token from .env
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+# Define Intents
+intents = discord.Intents.default()
+intents.message_content = True  
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} (ID: {bot.user.id})')
+    print('------')
+
+@bot.command()
+async def test(ctx):
+    await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
+
+bot.run(TOKEN)
